7ce6bc727354517a9cc27f502c477153dff4515b
eugene <yjjung1017@gmail.com>
2022-03-23 12:09:33 +0900

    edit

--- Algorithm/1869.py
+++ Algorithm/1869.py
@@ -1,7 +1,7 @@
 #2022-03-22 달팽이는 올라가고 싶다.
 
 a,b,v = map(int, input().split())
-#소요일 =
+#소요일 = (전체길이-내려오는 길이)/(올라가는길이-내려오는길이=총 올라가는길이)
 d=(v-b)/(a-b)
 if d==int(d):print(int(d))
 else: print(int(d)+1)
