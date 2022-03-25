625ef903a7eb1c803a51068840236278c93e5141
eugene <yjjung1017@gmail.com>
2022-03-26 01:26:03 +0900

    add method

--- baekjoon/2869.py
+++ baekjoon/2869.py
@@ -3,5 +3,14 @@
 a,b,v = map(int, input().split())
 #소요일 = (전체길이-내려오는 길이)/(올라가는길이-내려오는길이=총 올라가는길이)
 d=(v-b)/(a-b)
+f=(v-b)//(a-b)
+print(d)
+print(f)
+
+c=4.5
+print(c)
+d=int(c)
+print(d)
+
 if d==int(d):print(int(d))
 else: print(int(d)+1)
