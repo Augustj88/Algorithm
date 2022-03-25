922bb26fbcec850d04df886e90bedee1d2bf60e0
eugene <yjjung1017@gmail.com>
2022-03-26 02:49:52 +0900

    add method

--- goorm/43092.py
+++ goorm/43092.py
@@ -1,9 +1,23 @@
 #리그경기횟수구하기
 
 n = int(input())
-cnt = 0
 
-for i in range(1,n):
-    cnt += i
-    
-print(cnt)
+#method1
+cnt = 0
+if n < 2: #예외처리
+    print()
+else:
+    for i in range(1,n):
+        cnt += i
+    print(cnt)
+
+#method2
+def a():
+    cnt = 0
+    if n < 2:
+        print()
+    else:
+        for i in range(1,n):
+            cnt += i
+        return cnt
+print(a())
